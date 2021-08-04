c8c62ff4122929499a9c00aea9eef4bcb1c00d4c
Ben <benjaminb838@gmail.com>
2021-08-03 21:24:58 -0700

    adding ski resort landing page

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //Navbar animation
 const nav = document.querySelector("#navbar");
 const moon = document.querySelector(".moon-icon");
@@ -56,62 +55,3 @@ document.querySelector(".checkbtn").addEventListener("click", showMenu);
 // }
 
 // init();
-=======
-//Navbar animation
-const nav = document.querySelector("#navbar");
-const moon = document.querySelector(".moon-icon");
-const sun = document.querySelector(".sun-icon");
-
-nav.addEventListener("mouseover", function (e) {
-  if (e.target.classList.contains("nav-link")) {
-    const link = e.target;
-    const siblings = link.closest("#navbar").querySelectorAll(".nav-link");
-    siblings.forEach((el) => {
-      if (el !== link) el.style.opacity = 0.5;
-    });
-  }
-});
-
-nav.addEventListener("mouseout", function (e) {
-  if (e.target.classList.contains("nav-link")) {
-    const link = e.target;
-    const siblings = link.closest("#navbar").querySelectorAll(".nav-link");
-    siblings.forEach((el) => {
-      if (el !== link) el.style.opacity = 1;
-    });
-  }
-});
-
-//Dark Mode
-const toggleDark = function () {
-  document.body.classList.toggle("dark");
-};
-
-document.querySelector("input").addEventListener("click", toggleDark);
-
-// Hamburger menu
-const showMenu = function () {
-  const hamburgerMenu = document.querySelector("#navbar-nav");
-  hamburgerMenu.classList.toggle("show");
-};
-
-document.querySelector(".checkbtn").addEventListener("click", showMenu);
-
-//Page load Animation...Not used in final, keep for reference.
-// const loadScreen = document.querySelector(".load-screen");
-// const wrapper = document.querySelector(".wrapper");
-
-// function init() {
-//   setTimeout(() => {
-//     loadScreen.style.display = "none";
-//     loadScreen.style.opacity = 0;
-
-//     wrapper.style.display = "grid";
-//     wrapper.style.opacity = 1;
-
-//     setTimeout(() => (wrapper.style.opacity = 1), 50);
-//   }, 2000);
-// }
-
-// init();
->>>>>>> 9ca478b2d13b9bdee9e0df39efc9ebfcdc47d281
